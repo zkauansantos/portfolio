433e128caa12f789b47966da552f444cb2ef7f66
zkauansantos <kauansantosdepontes@gmail.com>
2023-02-09 12:45:25 -0300

    first commit

---
+++ src/assets/styles/GlobalStyles.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Inconsolata' sans-serif;
+    color: ${({ theme }) => theme.colors.text};
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors.lightBg};
+  }
+
+  a{
+    text-decoration: none;
+    color: #000;
+  }
+
+  ul{
+    list-style: none;
+  }
+`;
---
+++ src/assets/styles/theme/index.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    darkBg: '#1B1A17',
+    lightBg: '#242320',
+    details: '#A35709',
+    markText: '#FF8303',
+    text: '#F0E3CA',
+  },
+};
