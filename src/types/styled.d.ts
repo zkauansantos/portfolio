2a62fe78b51aa9653091860be57b0a57e0ca79fe
zkauansantos <kauansantosdepontes@gmail.com>
2023-02-09 12:46:49 -0300

    typed default theme

---
+++ src/types/styled.d.ts
@@ -0,0 +1,14 @@
+// styled.d.ts
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      darkBg: string,
+      lightBg: string
+      details: string,
+      markText: string,
+      text: string
+    }
+  }
+}
