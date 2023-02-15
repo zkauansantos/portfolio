4e3c6eb68bff36f7fe1b0a385b693f8032f5f311
zkauansantos <kauansantosdepontes@gmail.com>
2023-02-15 09:51:52 -0300

    criated footer with styles and responsive

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,67 @@
+import {
+  AiOutlineMail,
+  AiFillGithub,
+  AiOutlineWhatsApp,
+  AiFillLinkedin,
+} from 'react-icons/ai';
+import {
+  Contact, Container, Description, SocialLinks,
+} from './styles';
+
+export default function Footer() {
+  return (
+    <Container>
+      <Description>
+        <h3>LOGO</h3>
+
+        <p>
+          Se você gostou do meu trabalho e precisa de mim, entre em contato comigo.
+        </p>
+      </Description>
+
+      <Contact>
+        <h3>Contato</h3>
+
+        <SocialLinks>
+          <a
+            href='https://mail.google.com/mail/?view=cm&to=kauansantosdepontes@gmail.com'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <span><AiOutlineMail /></span>
+            kauansantosdepontes@gmail.com
+          </a>
+
+          <a
+            className='mg'
+            href='https://github.com/zkauansantos'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <span><AiFillGithub /></span>
+            zkauansantos
+          </a>
+
+          <a
+            href='https://linkedin/in/zkauansantos'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <span><AiOutlineWhatsApp /></span>
+            (61) 98255-2480
+          </a>
+
+          <a
+            className='mg'
+            href='https://linkedin.com/in/zkauansantos'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <span><AiFillLinkedin /></span>
+            in/zkauansantos
+          </a>
+        </SocialLinks>
+      </Contact>
+    </Container>
+  );
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,128 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+  padding: 64px 32px;
+  gap: 64px;
+  border-top: 1px solid  rgba(174, 218, 255, 0.3);
+
+  @media screen and (max-width: 700px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  @media screen and (max-width: 400px) {
+    gap: 28px;
+  }
+`;
+
+export const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 16px;
+
+  h3 {
+    font-size: 32px;
+    color: ${({ theme }) => theme.colors.markText};
+    padding-bottom: 2px;
+    border-bottom: 2px solid ${({ theme }) => theme.colors.details};
+  }
+
+  p {
+    font-size: 22px;
+    transition: width 0.2s ease-in;
+  }
+
+  @media screen and (max-width: 700px) {
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+
+    h3 {
+      width: 30%;
+    }
+
+    p {
+      max-width: 70%;
+    }
+  }
+
+  @media screen and (max-width: 400px) {
+    h3 {
+      font-size: 28px;
+    }
+
+    p {
+      font-size: 18px;
+    }
+  }
+`;
+
+export const Contact = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h3 {
+    color: ${({ theme }) => theme.colors.markText};
+    margin-bottom: 16px;
+    font-size: 32px;
+  }
+
+  @media screen and (max-width: 700px) {
+    align-items: center;
+  }
+
+  @media screen and (max-width: 400px) {
+    h3 {
+      font-size: 28px;
+    }
+  }
+`;
+
+export const SocialLinks = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+
+  .mg {
+    margin-left: 12px;
+  }
+
+  a{
+    color: ${({ theme }) => theme.colors.markText};
+    font-size: 22px;
+    display: flex;
+    align-items: center;
+    gap: 6px;
+    cursor: pointer;
+
+    span {
+      margin-top: 3px;
+    }
+  }
+
+  @media screen and (max-width:  940px) {
+    grid-template-columns: repeat(1, auto);
+    gap: 8px 4px;
+
+    .mg {
+      margin-left: 0;
+    }
+  }
+
+  @media screen and (max-width: 700px) {
+    text-align: center;
+    place-items: center;
+  }
+
+  @media screen and (max-width: 400px) {
+    a {
+      font-size: 18px;
+    }
+  }
+`;
